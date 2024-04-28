c69508d62a2cd68d3e55ebed33b0005fdc61e9d2
Elivelton <eliveltoncontact@gmail.com>
2024-04-28 17:32:15 -0300

    botar Voltar para o topo

---
+++ scripts/topo.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var botaoVoltarAoTopo = document.getElementById('voltarAoTopo');
+
+    window.onscroll = function() {
+        if (document.documentElement.scrollTop > 100) { // Mostrar o botão quando o usuário rolar 100 pixels para baixo
+            botaoVoltarAoTopo.style.display = 'block';
+        } else {
+            botaoVoltarAoTopo.style.display = 'none';
+        }
+    };
+
+    botaoVoltarAoTopo.onclick = function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Rolar suavemente
+        });
+    };
+});
