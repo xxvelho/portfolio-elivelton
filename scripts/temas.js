8fd0e42aaded839227a949ff0009768ff29b5f44
Elivelton <eliveltoncontact@gmail.com>
2024-05-06 20:30:06 -0300

    opcao de voltar para tema padrao

--- scripts/temas.js
+++ scripts/temas.js
@@ -3,17 +3,47 @@ const body = document.body;
 function ativarTema1() {
     body.classList.remove('tema-2');
     body.classList.remove('tema-3');
-    body.classList.add('tema-1');
+    if(body.classList.contains('tema-1')){
+        body.classList.remove('tema-1');
+        body.classList.add('tema-padrao');
+        let elemento = document.getElementById('tema-1');
+        elemento.style.background = '#163020';
+    }
+    else{
+        body.classList.add('tema-1');
+        let elemento = document.getElementById('tema-1');
+        elemento.style.background = '#241023';
+    }
 }
 
 function ativarTema2() {
     body.classList.remove('tema-1');
     body.classList.remove('tema-3');
-    body.classList.add('tema-2');
+    if(body.classList.contains('tema-2')){
+        body.classList.remove('tema-2');
+        body.classList.add('tema-padrao');
+        let elemento = document.getElementById('tema-2');
+        elemento.style.background = '#0C0C0C';
+    }
+    else{
+        body.classList.add('tema-2');
+        let elemento = document.getElementById('tema-2');
+        elemento.style.background = '#241023';
+    }
 }
 
 function ativarTema3() {
     body.classList.remove('tema-1');
     body.classList.remove('tema-2');
-    body.classList.add('tema-3');
+    if(body.classList.contains('tema-3')){
+        body.classList.remove('tema-3');
+        body.classList.add('tema-padrao');
+        let elemento = document.getElementById('tema-3');
+        elemento.style.background = '#860A35';
+    }
+    else{
+        body.classList.add('tema-3');
+        let elemento = document.getElementById('tema-3');
+        elemento.style.background = '#241023';
+    }
 }
